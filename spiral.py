739d4fda3e41e81d992ae9bee5b4795ac20b886f
Pro5507 <nhupendrakumar9162@gmail.com>
2024-09-19 18:41:32 +0530

    Turtle

---
+++ spiral.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.Screen().bgcolor("green")
+pen= turtle.Turtle()
+size= 0
+while True:
+    for i in range(4):
+        pen.fd(size+1)
+        pen.left(90)
+        size= size-5
+    size= size+1
+    
